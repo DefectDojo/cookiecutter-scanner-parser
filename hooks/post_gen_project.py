b957a7dc1c51efbb85f933297cf4674ad2c7cb90
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2021-03-29 13:43:25 -0400

    Changed preprocess hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,7 +16,7 @@ def update_json_file():
     maxNum = 0
     new_scanner_json = None
     tool_directory = "{{cookiecutter.tool_directory_name}}"
-    source = Path(WORKING).parent.absolute()
+    source = str(Path(WORKING).parent.absolute())
     destination = '../dojo/'
     filename = '../dojo/fixtures/test_type.json'
 
